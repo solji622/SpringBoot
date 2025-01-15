4528ffe4dc77d554474039f5ddb4bf4874201940
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-01-15 21:46:29 +0900

    질문 데이터 조회 - findBySubjectLike()
     QuestionRepository.java 확인하기

--- Doit/src/test/SbbApplicationTests.java
+++ Doit/src/test/SbbApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +16,9 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
