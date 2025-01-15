b0fe6e013f06fe3235d168bf0694abb3a26ee2e5
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-01-15 21:42:19 +0900

    질문 데이터 조회 - findAll()

--- Doit/src/test/SbbApplicationTests.java
+++ Doit/src/test/SbbApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +16,11 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size()); // 두개의 질문 데이터를 저장해서 데이터 사이즈는 2
 		
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
