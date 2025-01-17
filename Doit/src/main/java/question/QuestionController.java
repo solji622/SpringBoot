5c81f9513e4c53e959ce7926dbccbf6f113b2e03
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-01-17 22:45:46 +0900

    URL 프리픽스

--- Doit/src/main/java/question/QuestionController.java
+++ Doit/src/main/java/question/QuestionController.java
@@ -6,22 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionlist = this.questionService.getList();
 		model.addAttribute("questionList", questionlist);
 		return "question_list";
 	}
 	
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
