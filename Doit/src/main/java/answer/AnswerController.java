15c52635965a1d95df9d9df5da2272c494b3acc0
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-01-27 16:16:37 +0900

    답변 등록 기능 추가를 위한 폼 클래스 생성과 컨트롤러 수정

--- Doit/src/main/java/answer/AnswerController.java
+++ Doit/src/main/java/answer/AnswerController.java
@@ -2,6 +2,7 @@ package com.example.demo.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.question.Question;
 import com.example.demo.question.QuestionService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/answer")
@@ -21,9 +23,14 @@ public class AnswerController {
 	
 	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id,
-			@RequestParam(value="content") String content) {
+			@Valid AnswerForm answerForm,
+			BindingResult bindingResult) {
 		Question question = this.questionService.getQuestion(id);
-		this.answerService.create(question, content);
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
 }
