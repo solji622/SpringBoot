2f4f59ca0b1ef85983046da0a708bcced6ba6968
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-01-29 21:45:57 +0900

    페이징 기능 구현
    Repository - 메서드 구현
    Controller - URL 매핑
    Service - 보여줄 게시물 개수 제한

--- Doit/src/main/java/question/QuestionController.java
+++ Doit/src/main/java/question/QuestionController.java
@@ -1,7 +1,6 @@
 package com.example.demo.question;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,9 +22,9 @@ public class QuestionController {
 	private final QuestionService questionService;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Question> questionlist = this.questionService.getList();
-		model.addAttribute("questionList", questionlist);
+	public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+		Page<Question> paging = this.questionService.getList(page);
+		model.addAttribute("paging", paging);
 		return "question_list";
 	}
 	
