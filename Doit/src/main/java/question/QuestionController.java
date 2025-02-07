752d8bc4155316b29b5857507accc37451838477
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-02-07 15:49:27 +0900

    principal 객체 사용하여 글쓴이 정보 저장

--- Doit/src/main/java/question/QuestionController.java
+++ Doit/src/main/java/question/QuestionController.java
@@ -1,6 +1,9 @@
 package com.example.demo.question;
 
+import java.security.Principal;
+
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.answer.AnswerForm;
+import com.example.demo.user.SiteUser;
+import com.example.demo.user.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class QuestionController {
 	private final QuestionService questionService;
+	private final UserService userService;
 	
 	@GetMapping("/list")
 	public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
@@ -36,19 +42,23 @@ public class QuestionController {
 		return "question_detail";
 	}
 	
+	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/create")
 	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
+	@PreAuthorize("isAuthenticated()")
 	@PostMapping("/create")
 	public String questionCreate(@Valid QuestionForm questionForm,
-			BindingResult bindingResult) {
+			BindingResult bindingResult,
+			Principal principal) {
 		if (bindingResult.hasErrors()) {
 			return "question_form";
 		}
+		SiteUser siteUser = this.userService.getUser(principal.getName());
 		this.questionService.create(questionForm.getSubject(), 
-				questionForm.getContent());
+				questionForm.getContent(), siteUser);
 		return "redirect:/question/list";
 	}
 }
