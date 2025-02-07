82438ee81328c685758bde0b18411d984b4cbc2a
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-02-07 15:42:12 +0900

    siteuser 엔티티 조회

--- Doit/src/main/java/user/UserRepository.java
+++ Doit/src/main/java/user/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
 	
-
+	Optional<SiteUser> findByUsername(String username);
 }
