605d735a4b27aa684abaf7449b5f4ec85488eba8
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-02-07 15:55:06 +0900

    기능 추가
    로그인 URL 등록
    시큐리티 인증 처리용 빈 생성
    로그아웃 기능 구현
    @PreAuthorize("isAuthenticated()") 동작을 위한 설정 수정

--- Doit/src/main/SecurityConfig.java
+++ Doit/src/main/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.net.Authenticator;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +17,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,12 +28,27 @@ public class SecurityConfig {
 					(new AntPathRequestMatcher("/h2-console/**")))
 			.headers((headers) -> headers
 					.addHeaderWriter(new XFrameOptionsHeaderWriter(
-							XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+							XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+			.formLogin((formLogin) -> formLogin
+					.loginPage("/user/login")
+					.defaultSuccessUrl("/"))
+			.logout((logout) -> logout
+				.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true))
+			;
+		
 		return http.build();
 	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration
+	authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
