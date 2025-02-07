48fbda1dda7b2cdb24e2b66e3137830625a93f26
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-02-07 15:40:26 +0900

    로그인 URL 맵핑

--- Doit/src/main/java/user/UserController.java
+++ Doit/src/main/java/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -46,4 +48,10 @@ public class UserController {
 		
 		return "redirect:/";
 	}
+	
+	@GetMapping("/login") 
+	public String login(){
+		return "login_form";
+	}
+	
 }
