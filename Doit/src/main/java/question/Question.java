dbfe399818133736c2d1a510d47c52d1831c908b
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2025-02-07 15:44:53 +0900

    Add files via upload

--- Doit/src/main/java/question/Question.java
+++ Doit/src/main/java/question/Question.java
@@ -1,7 +1,10 @@
-package com.example.demo;
+package com.example.demo.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import com.example.demo.answer.Answer;
+import com.example.demo.user.SiteUser;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -9,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,4 +35,7 @@ public class Question {
 	
 	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
 	private List<Answer> answerList;
+	
+	@ManyToOne
+	private SiteUser author;
 }
